3723932c789c9903b96ef64bf9fc2610083f2135
jose.poot <jose-9803@hotmail.com>
2023-12-20 12:24:41 -0500

    add filter by Tittle

---
+++ src/components/addBlog.tsx
@@ -0,0 +1,113 @@
+import React, { useState } from 'react';
+import { TextField, Button, Grid, Paper, Typography } from '@mui/material';
+
+const Addblog = () => {
+  const [formData, setFormData]:any = useState({
+    Titulo: '',
+    Autor: '',
+    Fecha: '',
+    Contenido: '',
+  });
+
+  const [formErrors, setFormErrors] = useState({
+    Titulo: '',
+    Autor: '',
+    Fecha: '',
+    Contenido: '',
+  });
+
+  const handleChange = (e:any) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+    setFormErrors({ ...formErrors, [name]: '' });
+  };
+
+  const handleSubmit = (e:any) => {
+    e.preventDefault();
+
+    // Validar campos obligatorios
+    const errors:any = {};
+    Object.keys(formData).forEach((key) => {
+      if (formData[key].trim() === '') {
+        errors[key] = 'Este campo es obligatorio';
+      }
+    });
+
+    if (Object.keys(errors).length > 0) {
+      setFormErrors(errors);
+    } else {
+      // Enviar el formulario o realizar otras acciones
+      console.log('Formulario enviado:', formData);
+    }
+  };
+
+  return (
+    <Grid container justifyContent="center" alignItems="center" height="100vh">
+      <Grid item xs={10} sm={6} md={4}>
+        <Paper elevation={3} style={{ padding: 16 }}>
+          <Typography variant="h5" align="center" gutterBottom>
+            Datos de entrada
+          </Typography>
+
+          <form onSubmit={handleSubmit}>
+            <TextField
+              label="Nombre"
+              name="Titulo"
+              fullWidth
+              margin="normal"
+              value={formData.Titulo}
+              onChange={handleChange}
+              error={Boolean(formErrors.Titulo)}
+              helperText={formErrors.Titulo}
+              required
+            />
+
+            <TextField
+              label="Apellido"
+              name="Autor"
+              fullWidth
+              margin="normal"
+              value={formData.Autor}
+              onChange={handleChange}
+              error={Boolean(formErrors.Autor)}
+              helperText={formErrors.Autor}
+              required
+            />
+
+            <TextField
+              label="Correo Electrónico"
+              type="Fecha"
+              name="Fecha"
+              fullWidth
+              margin="normal"
+              value={formData.Fecha}
+              onChange={handleChange}
+              error={Boolean(formErrors.Fecha)}
+              helperText={formErrors.Fecha}
+              required
+            />
+
+            <TextField
+              label="Contraseña"
+              type="Contenido"
+              name="Contenido"
+              fullWidth
+              margin="normal"
+              value={formData.Contenido}
+              onChange={handleChange}
+              error={Boolean(formErrors.Contenido)}
+              helperText={formErrors.Contenido}
+              required
+            />
+
+            <Button type="submit" variant="contained" color="primary" fullWidth style={{ marginTop: 16 }}>
+              Guardar
+            </Button>
+          </form>
+        </Paper>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Addblog;
--- src/components/list.tsx
+++ src/components/list.tsx
@@ -1,53 +1,89 @@
-import { Button, Grid, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import {
+  Button,
+  Grid,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TextField,
+} from "@mui/material";
+import { useState } from "react";
 
-
-const List: React.FC = ()=>{
-const array:Array<any>=[
+const List: React.FC = () => {
+  const array: Array<any> = [
     {
-        Titulo:'Iro man 2',
-        Autor:'Test',
-        Fecha:'text',
-        Contedio:'test testteadsañdsajlkdjfkjdlñfjsdjñfdjslñ'
+      Titulo: "Iro man 2",
+      Autor: "Test",
+      Fecha: "text",
+      Contenido: "test testteadsañdsajlkdjfkjdlñfjsdjñfdjslñ",
     },
     {
-        Titulo:'Iro man 3',
-        Autor:'Test2',
-        Fecha:'text2',
-        Contedio:'test testteadsañdsajlkdjfkjdlñfjsdjñfdjslñ'
-    }
-]
-    return(
-        <>
-         <Grid container justifyContent="flex-end">
-            <Button variant="contained" color="primary">
-                Mi Botón
-            </Button>
-        </Grid>
-        <TableContainer component={Paper}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Titulo</TableCell>
-            <TableCell>Autor</TableCell>
-            <TableCell>Fecha</TableCell>
-            <TableCell>Contedio</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {array.map((movie, index) => (
-            <TableRow key={index}>
-              <TableCell>{movie.Titulo}</TableCell>
-              <TableCell>{movie.Autor}</TableCell>
-              <TableCell>{movie.Fecha}</TableCell>
-              <TableCell>{movie.Contedio}</TableCell>
+      Titulo: "Iro man 3",
+      Autor: "Test2",
+      Fecha: "text2",
+      Contenido: "test testteadsañdsajlkdjfkjdlñfjsdjñfdjslñ",
+    },
+    {
+      Titulo: "hulk",
+      Autor: "Test2",
+      Fecha: "text2",
+      Contenido: "test testteadsañdsajlkdjfkjdlñfjsdjñfdjslñ",
+    },
+  ];
+  const [filter, setFilter] = useState('');
+
+  const filteredData = array.filter(item =>{
+    item.Contenido = item.Contenido.slice(0, 70);
+    return item.Titulo.toLowerCase().includes(filter.toLowerCase())
+
+  }
+  );
+  return (
+    <>
+      <Grid container justifyContent="flex-end">
+        <Button
+          variant="contained"
+          color="primary"
+          style={{ marginBottom: "20px" }}
+        >
+          Agregar entrada
+        </Button>
+      </Grid>
+      <TextField
+        label="Filtrar por titulo"
+        variant="outlined"
+        fullWidth
+        value={filter}
+        onChange={(e) => setFilter(e.target.value)}
+        style={{ marginBottom: 16 }}
+      />
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Titulo</TableCell>
+              <TableCell>Autor</TableCell>
+              <TableCell>Fecha</TableCell>
+              <TableCell>Contenido</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {filteredData.map((movie, index) => (
+              <TableRow key={index}>
+                <TableCell>{movie.Titulo}</TableCell>
+                <TableCell>{movie.Autor}</TableCell>
+                <TableCell>{movie.Fecha}</TableCell>
+                <TableCell>{movie.Contenido}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
   );
 };
-
 
 export default List;
